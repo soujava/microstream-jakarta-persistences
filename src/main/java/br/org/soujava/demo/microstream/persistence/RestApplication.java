1e8754baebb97d7f10ab5d1f1638b535b2de7516
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-04 11:47:13 +0100

    refactor: update rest appliction name
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/br/org/soujava/demo/microstream/persistence/RestApplication.java
+++ src/main/java/br/org/soujava/demo/microstream/persistence/RestApplication.java
@@ -3,9 +3,6 @@ package br.org.soujava.demo.microstream.persistence;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-/**
- *
- */
 @ApplicationPath("/")
-public class MicrostreampersistenceRestApplication extends Application {
+public class RestApplication extends Application {
 }
