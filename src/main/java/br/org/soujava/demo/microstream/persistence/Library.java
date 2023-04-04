b3633b77e3296ed243b1f00c81e7706fe202de2d
Otavio Santana <otaviopolianasantana@gmail.com>
2023-04-04 12:00:39 +0100

    feat: update library to have resource methods
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/br/org/soujava/demo/microstream/persistence/Library.java
+++ src/main/java/br/org/soujava/demo/microstream/persistence/Library.java
@@ -7,7 +7,5 @@ import java.util.List;
 
 @Repository
 public interface Library extends CrudRepository<Book, String> {
-    void deleteByTitle(String title);
-
     List<Book> findByTitle(String title);
 }
